d14100fdc2a780fc19273e7978436a5169da6282
devaraujstein <andreearaujo98@gmail.com>
2020-10-02 15:25:51 -0300

    Lidando com dados

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,43 @@
+import { GetServerSideProps } from 'next';
 import { Title } from '../styles/pages/home';
 
-export default function Home() {
+interface IProduct {
+  id: string;
+  title: string;
+}
+
+interface HomeProps {
+  recommendedProducts: IProduct[];
+}
+
+export default function Home({recommendedProducts}: HomeProps) {
   return (
     <div>
-      <Title>Suavão?</Title>
+      <section>
+        <Title>Suavão?</Title>
+
+        <ul>
+          {recommendedProducts.map(recommendedProduct => {
+            return (
+              <li key={recommendedProduct.id}>
+                {recommendedProduct.title}
+              </li>
+            )
+          })}
+        </ul>
+      </section>
+      
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+  const response = await fetch('http://localhost:3333/recommended');
+  const recommendedProducts = await response.json();
+
+  return {
+    props: {
+      recommendedProducts
+    } 
+  }
 }
